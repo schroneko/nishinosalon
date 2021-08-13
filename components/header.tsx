5f72fa744a7fd5c255e5d94d0d1fa9a518e92a69
schroneko <schroneko.fuller@gmail.com>
2021-08-14 00:24:12 +0900

    fix: hide signed in user info

--- components/header.tsx
+++ components/header.tsx
@@ -39,7 +39,7 @@ export default function Header() {
           )}
           {session && (
             <>
-              {session.user.image && (
+              {/* {session.user.image && (
                 <span
                   style={{ backgroundImage: `url(${session.user.image})` }}
                   className={styles.avatar}
@@ -49,7 +49,7 @@ export default function Header() {
                 <small>Signed in as</small>
                 <br />
                 <strong>{session.user.email || session.user.name}</strong>
-              </span>
+              </span> */}
               <a
                 href={`/api/auth/signout`}
                 className={styles.button}
